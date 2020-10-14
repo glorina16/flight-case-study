e9785a232cb4a0b771f5b652c87ec7fc5e4426f8
glorina16 <72781039+glorina16@users.noreply.github.com>
2020-10-14 17:06:49 +0530

    Add files via upload

---
+++ src/main/java/com/afkl/cases/df/TokenResponse.java
@@ -0,0 +1,24 @@
+package com.afkl.cases.df;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TokenResponse {
+    @JsonProperty("access_token")
+    private String accessToken;
+    @JsonProperty("token_type")
+    private String tokenType;
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+    @JsonProperty("expires_in")
+    private Integer expiresIn;
+    @JsonProperty("scope")
+    private String scope;
+    @JsonProperty("jti")
+    private String jti;
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+}
