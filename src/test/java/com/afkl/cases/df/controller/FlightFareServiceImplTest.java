e01df6849e32bc58085f0840705454929f26724d
Glorina16 <glorina1601@gmail.com>
2020-10-21 10:23:54 +0530

    test package

--- src/test/java/com/afkl/cases/df/controller/FlightFareServiceImplTest.java
+++ src/test/java/com/afkl/cases/df/controller/FlightFareServiceImplTest.java
@@ -1,24 +1,7 @@
-/*
 package com.afkl.cases.df.controller;
 
-import java.io.IOException;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
 
-*/
-/**
- * {@code BookControllerFunctionalTests} represents functional tests for
- * {@code BookController}.
- *
- *//*
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = {Application.class},
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("test")
-public class FlightFareServiceImplTests {
+/*public class FlightFareServiceImplTests {
 
 
-}
-*/
+}*/
